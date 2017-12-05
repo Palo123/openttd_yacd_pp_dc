a38d968781ca12722c560416d02426323cfb2545
Palo123 <palo_123@azet.sk>
2017-12-05 11:48:47 +0100

    r25342

--- openttd/src/economy.cpp
+++ openttd/src/economy.cpp
@@ -49,6 +49,7 @@
 #include "game/game.hpp"
 #include "cargomonitor.h"
 #include "goal_base.h"
+#include "story_base.h"
 #include "cargodest_func.h"
 
 #include "table/strings.h"
@@ -518,7 +519,7 @@ void ChangeOwnershipOfCompanyItems(Owner old_owner, Owner new_owner)
 		if (si->owner == old_owner) si->owner = new_owner == INVALID_OWNER ? OWNER_NONE : new_owner;
 	}
 
-	/* Remove Game Script created Goals and CargoMonitors. */
+	/* Remove Game Script created Goals, CargoMonitors and Story pages. */
 	Goal *g;
 	FOR_ALL_GOALS(g) {
 		if (g->company == old_owner) delete g;
@@ -526,6 +527,11 @@ void ChangeOwnershipOfCompanyItems(Owner old_owner, Owner new_owner)
 
 	ClearCargoPickupMonitoring(old_owner);
 	ClearCargoDeliveryMonitoring(old_owner);
+
+	StoryPage *sp;
+	FOR_ALL_STORY_PAGES(sp) {
+		if (sp->company == old_owner) delete sp;
+	}
 
 	/* Change colour of existing windows */
 	if (new_owner != INVALID_OWNER) ChangeWindowOwner(old_owner, new_owner);
