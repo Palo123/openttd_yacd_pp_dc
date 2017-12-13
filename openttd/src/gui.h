627a9fee0dd7d2f12489f9c56f09bde2548ced0b
Palo123 <palo_123@azet.sk>
2017-12-13 23:18:26 +0100

    r25352

--- openttd/src/gui.h
+++ openttd/src/gui.h
@@ -16,6 +16,7 @@
 #include "economy_type.h"
 #include "tile_type.h"
 #include "transport_type.h"
+#include "story_type.h"
 
 struct Window;
 
@@ -54,7 +55,7 @@ void ShowIndustryCargoesWindow();
 void ShowSubsidiesList();
 void ShowGoalsList();
 void ShowGoalQuestion(uint16 id, byte type, uint32 button_mask, const char *question);
-void ShowStoryBook();
+void ShowStoryBook(uint16 page_id = INVALID_STORY_PAGE);
 
 void ShowEstimatedCostOrIncome(Money cost, int x, int y);
 
